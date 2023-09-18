57ff7888c965baf9061d500964c193cff9e83e4c
Yoichi Ojima <yoichiojima@gmail.com>
2023-09-18 22:19:41 +0900

    update

--- calculate_income.cpp
+++ calculate_income.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int main() {
-    int sales = 95000;
+    double sales = 95000;
     double state_tax = 0.04;
     double country_tax = 0.02;
 
     double state_tax_to_pay = sales * state_tax;
     double country_tax_to_pay = sales * country_tax;
+    double income = sales - country_tax_to_pay - state_tax_to_pay;
 
     cout << "country tax: " << country_tax_to_pay << endl;
     cout << "state_tax: " << state_tax_to_pay << endl;
-    cout << "income: " << sales - country_tax_to_pay - state_tax_to_pay;
+    cout << "income: " << income;
 
     return 0;
 }

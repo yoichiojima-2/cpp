6ec1f5d9dd7adb8649672b5ad7541b5c4cc8a954
Yoichi Ojima <yoichiojima@gmail.com>
2023-09-20 20:21:45 +0900

    update

---
+++ find_the_highest_attitude.cpp
@@ -0,0 +1,21 @@
+// 1732. Find the Highest Altitude
+
+// There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+// The biker starts his trip on point 0 with altitude equal 0.
+// You are given an integer array gain of length n where gain[i] is the net gain in altitude
+// between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int idx = 0;
+    int attitude = 0;
+    int max_attitude = 0;
+
+    cout << idx << ", " << attitude << ", " << max_attitude << endl;
+
+    return 0;
+}
